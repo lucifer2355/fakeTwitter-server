37161679f6eb5adf31b0df0ad6da1b128079e077
lucifer2355 <dgajjar999@gmail.com>
2021-07-05 19:38:05 +0530

    add login muatation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,22 +2,70 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
 
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../utils/validators");
 const User = require("../../model/User");
+
+const generateToken = (user) =>
+  jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    process.env.SECRET_KEY,
+    { expiresIn: "1h" }
+  );
 
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      const user = await User.findOne({ username });
+      if (!user) {
+        errors.general = "User not found!";
+        throw new UserInputError("User not found!", { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong credential!";
+        throw new UserInputError("Wrong credential!", { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+
     async register(
       parent,
       { registerInput: { username, email, password, confirmPassword } }
     ) {
-      // TODO Validate user data
-      // TODO Make sure user doesn't already exit
+      //! Validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) throw new UserInputError("Errors", { errors });
+
+      //! Make sure user doesn't already exit
       const user = await User.findOne({ username });
       if (user)
         throw new UserInputError("Username is taken", {
           errors: { username: "This username is taken" },
         });
-      // TODO Hash password and create an auth token
+
+      //! Hash password and create an auth token
       password = await bcrypt.hash(password, 12);
 
       const newUser = new User({
@@ -29,15 +77,7 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        process.env.SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = generateToken(res);
 
       return {
         ...res._doc,
