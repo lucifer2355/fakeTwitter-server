5cf84c4cf12179cc73f1716d59112744e61cd563
lucifer2355 <dgajjar999@gmail.com>
2021-07-05 22:06:21 +0530

    add delete post mutation

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require("apollo-server");
 const Post = require("../../model/Post");
 const checkAuth = require("../../utils/checkAuth");
 
@@ -37,6 +38,22 @@ module.exports = {
       const post = await newPost.save();
 
       return post;
+    },
+
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post deleted successfully";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch (error) {
+        throw new Error(error);
+      }
     },
   },
 };
