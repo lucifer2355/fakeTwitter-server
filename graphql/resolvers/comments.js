85b037a84d637264ddd9e90cc3de93417eb60ac1
lucifer2355 <dgajjar999@gmail.com>
2021-07-06 17:47:03 +0530

    add like & unlike post mutation

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -40,5 +40,25 @@ module.exports = {
         } else throw new AuthenticationError("Action not allowed");
       } else throw new UserInputError("Post not found");
     },
+
+    likePost: async (_, { postId }, context) => {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          //! Post already like, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          post.likes.push({
+            username,
+            createAt: new Date().toISOString(),
+          });
+        }
+
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
+    },
   },
 };
